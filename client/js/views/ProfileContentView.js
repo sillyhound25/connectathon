1875787720f17809318270af98d77d88044c032a
David Hay <david.hay25@gmail.com>
2014-03-21 15:39:55 +1300

    just before OS upgrade

---
+++ client/js/views/ProfileContentView.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: davidha
+ * Date: 21/03/14
+ * Time: 1:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ server/medicationadministration.js
@@ -0,0 +1,56 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: davidha
+ * Date: 21/02/14
+ * Time: 10:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+var _ = require("underscore");
+var moment = require('moment');
+var builder = {};
+builder.common = require('./common.js');
+
+//get a sample in simple format
+function getSample(vo) {
+    var entry = {};
+    entry.title='Encounter';
+    entry.id = 'cid:Encounter'+ new Date().getTime();
+    entry.updated = moment().format();
+
+    var sam = {};
+    sam.resourceType = "Encounter";
+    sam.status = 'in progress';
+    sam.class = 'inpatient';
+    sam.identifier = [{value:"ZZZ",system:"http://myhospital/ID"}];
+    sam.text = getText(this);
+    sam.subject = {reference:vo.patientID};
+
+
+    var participant = {individual : {reference:vo.practitionerID},
+        type: [builder.common.cc({code:'CON',display:'Consultant',system:'http://hl7.org/fhir/v3/ParticipationType'})]};
+    sam.participant = [participant];
+
+    entry.content = sam;
+    return entry;
+}
+
+//generate the text element for the resource...
+function getText(obj) {
+    var rtn = {status:"generated"};
+    var txt = "MyHospital admission";
+    /*
+    txt += "<div>Identifiers</div><ul>";
+    _.each(obj.identifier,function(ident){
+        txt += "<li>Use:" + ident.use + ": " + ident.value + " (" + ident.system + ")</li>"
+    })
+    txt += "</ul>";
+    */
+    rtn.div = "<div>" + txt + "</div>";
+    return rtn;
+}
+
+
+exports.getSample = getSample;
+exports.getDependencies = function() {
+    return ['patient','practitioner']
+}
