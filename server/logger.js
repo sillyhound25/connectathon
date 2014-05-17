efa7418c78b5ff8285b5ce7bff65d655e5f37164
sillyhound25 <sillyhound25@gmail.com>
2014-05-18 09:42:47 +1200

    basic log functionity working

--- server/logger.js
+++ server/logger.js
@@ -4,25 +4,42 @@
 
 /* global exports,require */
 
+
 var _ = require("underscore");
+var ObjectID = require('mongodb').ObjectID;
 
 var mongoDbCon;
 
 exports.addRoutes = function(app){
+
+    //return a single log entry
+    app.get('/admin/log/detail/:id', function(req, res) {
+        var id = req.params.id;
+        var collection = mongoDbCon.collection('RESTLog');
+        collection.findOne({_id:ObjectID(id)},function(err,doc){
+            res.json(doc);
+        });
+    });
+
     //return the log summary
     app.get('/admin/log/summary', function(req, res) {
         var collection = mongoDbCon.collection('RESTLog');
 
         var ar = [];
         var cursor = collection.find();
-        //cursor.sort(fields).limit(n).skip(m).
         cursor.sort({time:-1}).each(function(err,doc){
             //console.log(err,doc);
             if (doc){
                 var item = {time:doc.time,method:doc.method,uri:doc.uri,statusCode:doc.statusCode,id:doc._id};
                 if (doc.statusCode >= 300) {
                     //if there was an error, then add additional items...
-                    item.body = JSON.parse(doc.body);       //what the server send back
+                    //note that if the body is not valid json, then return it as a string in a json element
+                    try {
+                        item.body = JSON.parse(doc.body);       //what the server send back
+                    } catch (ex) {
+                        item.body = {body:doc.body};
+                    }
+
                     item.resource = doc.savedResource;  //what we were trying to save (for a POST/PUT)
                 }
                 ar.push(item);
